042b5c9d957c4636848e308205683b03c0a38394
Fragas <141639158+FragasOficial@users.noreply.github.com>
2025-06-16 09:31:28 -0300

    Re fatorando código
    Alteração nos códigos

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-document.getElementById('form-contato').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
+document.getElementById('form-contato').addEventListener('submit', function(e) {
+    e.preventDefault();
     const status = document.getElementById('mensagem-status');
-    status.textContent = "Mensagem enviada com sucesso! Entraremos em contato em breve.";
+    status.textContent = "Sua mensagem foi enviada! Retornaremos em breve.";
     status.style.color = "#bfa048";
-    
-    // Resetar formulário (simulação)
     this.reset();
 });
